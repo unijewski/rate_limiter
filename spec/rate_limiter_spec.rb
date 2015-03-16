a9186b4c9be6c961649426d71173b401f5e83f86
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-03-16 20:22:09 +0100

    [#2] Spec for manual setting x-ratelimit header

--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'pry'
 
 describe RateLimiter do
   let(:app) { proc { ['200', { 'Content-Type' => 'text/html' }, ['Hello world']] } }
@@ -16,6 +15,13 @@ describe RateLimiter do
     it 'should contain headers' do
       expect(response.headers).to include('X-RateLimit-Limit')
       expect(response.headers['X-RateLimit-Limit']).to eq 60
+    end
+
+    context 'with a different value for X-RateLimit' do
+      it 'should contain the proper limit' do
+        response.headers['X-RateLimit-Limit'] = 40
+        expect(response.headers['X-RateLimit-Limit']).to eq 40
+      end
     end
   end
 end
