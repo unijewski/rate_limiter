58a505912ceafae6c8162cb8022b149a3f6eb58c
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-03-16 20:07:48 +0100

    [#2] Updated specs including headers

--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
+require 'pry'
 
 describe RateLimiter do
   let(:app) { proc { ['200', { 'Content-Type' => 'text/html' }, ['Hello world']] } }
+  let(:stack) { RateLimiter.new(app) }
+  let(:request) { Rack::MockRequest.new(stack) }
+  let(:response) { request.get('/') }
 
-  it 'should respond successfully' do
-    get '/'
-    expect(last_response.status).to eq 200
-    expect(last_response.body).to eq 'Hello world'
+  context 'when we send a request' do
+    it 'should respond successfully' do
+      expect(response.status).to eq 200
+      expect(response.body).to eq 'Hello world'
+    end
+
+    it 'should contain headers' do
+      expect(response.headers).to include('X-RateLimit-Limit')
+      expect(response.headers['X-RateLimit-Limit']).to eq 60
+    end
   end
 end
