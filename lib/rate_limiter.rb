88cd9d2014606969544aeb5651964bffec330fa9
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-03-17 19:31:38 +0100

    [#4] Decreasing x-rate limit

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -1,19 +1,30 @@
 require 'rate_limiter/version'
+require 'pry'
 
 class RateLimiter
   def initialize(app, options = {})
     @app = app
     @options = options
+    xratelimit = @options[:limit] || 60
+    @xratelimit = xratelimit.to_i
+    @remaining_xratelimit = @xratelimit
   end
 
   def call(env)
     status, headers, body = @app.call(env)
-     change_xratelimit(headers)
+    define_headers(headers)
     [status, headers, body]
   end
 
-  def change_xratelimit(headers)
-    value = @options[:limit] || '60'
-    headers.merge!('X-RateLimit-Limit' => value)
+  private
+
+  def define_headers(headers)
+    decrease_xratelimit
+    headers.merge!('X-RateLimit-Limit' => @xratelimit.to_s)
+    headers.merge!('X-RateLimit-Remaining' => @remaining_xratelimit.to_s)
+  end
+
+  def decrease_xratelimit
+    @remaining_xratelimit -= 1
   end
 end
