b0358e1402b49ca5ab766e86ad776fc42814e1c6
Grzegorz Unijewski <grzegorz@unijewski.pl>
2015-03-18 11:52:33 +0100

    Specs refactoring

--- spec/rate_limiter_spec.rb
+++ spec/rate_limiter_spec.rb
@@ -1,47 +1,49 @@
 require 'spec_helper'
 
 describe RateLimiter do
-  let(:app) { proc { ['200', { 'Content-Type' => 'text/html' }, ['Hello world']] } }
-  let(:wrapper) { Rack::Lint.new(stack) }
-  let(:request) { Rack::MockRequest.new(wrapper) }
-  let(:response) { request.get('/') }
+  let(:info) { proc { ['200', { 'Content-Type' => 'text/html' }, ['Hello world']] } }
+  let(:wrapper) { Rack::Lint.new(app) }
 
   context 'when we send a request without options' do
-    let(:stack) { RateLimiter.new(app) }
+    let(:app) { RateLimiter.new(info) }
+
+    before { get('/') }
 
     it 'should respond successfully' do
-      expect(response.status).to eq 200
-      expect(response.body).to eq 'Hello world'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'Hello world'
     end
 
     it 'should contain headers' do
-      expect(response.headers).to include('X-RateLimit-Limit')
-      expect(response.headers).to include('X-RateLimit-Remaining')
-      expect(response.headers['X-RateLimit-Limit']).to eq '60'
+      expect(last_response.headers).to include('X-RateLimit-Limit')
+      expect(last_response.headers).to include('X-RateLimit-Remaining')
+      expect(last_response.headers['X-RateLimit-Limit']).to eq '60'
     end
   end
 
   context 'when we send requests with options' do
-    let(:stack) { RateLimiter.new(app, limit: '10') }
+    let(:app) { RateLimiter.new(info, limit: '10') }
+
+    before { get('/') }
 
     it 'should contain the proper limit' do
-      expect(response.headers['X-RateLimit-Limit']).to eq '10'
+      expect(last_response.headers['X-RateLimit-Limit']).to eq '10'
     end
 
     context '5 times in total' do
-      before { 4.times { request.get('/') } }
+      before { 4.times { get('/') } }
 
       it 'should decrease X-RateLimit-Remaining' do
-        expect(response.headers['X-RateLimit-Remaining']).to eq '5'
+        expect(last_response.headers['X-RateLimit-Remaining']).to eq '5'
       end
     end
 
     context 'the limit was reached' do
-      before { 12.times { request.get('/') } }
+      before { 12.times { get('/') } }
 
       it 'should get the 429 status code' do
-        expect(response.status).to eq 429
-        expect(stack).not_to receive(:call)
+        expect(last_response.status).to eq 429
+        expect(app).not_to receive(:call)
       end
     end
   end
